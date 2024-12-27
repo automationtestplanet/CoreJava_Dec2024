dbc9c7780c570ac3563f91c77370420c0e7271a7
RAJU CHELLE <rajuchelle@gmail.com>
2024-12-27 09:24:03 +0530

    assignments

---
+++ src/assignments/raju/Assignment1.java
@@ -0,0 +1,13 @@
+package assignments.raju;
+
+public class Assignment1 {
+	
+	//Write a reusable program to print Even and odd numbers count from an array 
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
