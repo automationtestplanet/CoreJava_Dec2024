bc081cb61d2a8b3a79f558c72957cb7d8b7f5517
Padamati Sasidhar Goud -X (padgoud - TECH MAHINDRA LIM at Cisco) <padgoud@cisco.com>
2024-12-28 03:33:37 +0530

    Assignment completed

--- src/assignments/sasidhar/Assignment1.java
+++ src/assignments/sasidhar/Assignment1.java
@@ -1,16 +1,41 @@
 package assignments.sasidhar;
 
+import java.util.Scanner;
+
 public class Assignment1 {
 
-	
-	// Write a program to reverse a string without reversing the special characters position
-	// Input: "ab@cde%fg"
-	// Output: "gf@edc%ba"
-	
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter a string: "); 
+		String Input = scn.nextLine();
+		scn.close();
+		System.out.print("INPUT ="+Input+"\n"); 
+		String Output = reverseString(Input);
+		System.out.println("OUTPUT ="+Output);
+	}
+	public static String reverseString(String Input) {
+		char[] Characters = Input.toCharArray();
+		
+		int L= 0;
+		int R= Characters.length - 1;
+		
+		while(L<R) {
+			if(!Character.isLetterOrDigit(Characters[L])) {
+				L++;
+			} else if(!Character.isLetterOrDigit(Characters[R])){
+				R--;
+			}
+			else {
+				char temp = Characters[L];
+				Characters[L] = Characters[R];
+				Characters[R] = temp;
+				L++;
+                R--;
+			}
+		}
+		return new String(Characters);
 	}
 
 }
+
